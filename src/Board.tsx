e5642b1a3dafd031bfa4f917615595f2d8e2967b
Dylan <dylananson144@gmail.com>
2024-08-11 13:02:32 +1000

    feat: tailwind working now

---
+++ src/Board.tsx
@@ -0,0 +1,12 @@
+function Board() {
+
+    return (
+        <>
+            <div className="text-red-50 border-black b-2 bg-black">
+                Hello world this is a board
+            </div>
+        </>
+    )
+}
+
+export default Board
