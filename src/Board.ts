4f1039acc9c032bef8c6c570b701754f678df4e7
Dylan <dylananson144@gmail.com>
2024-08-25 12:19:49 +1000

    refactor: move some stuff

--- src/Board.ts
+++ src/Board.ts
@@ -24,40 +24,6 @@ export type NewBoard = {
     hasLegalMove: (color: Color) => boolean
 }
 
-export const emptyRow = (): Row<undefined> => {
-    return Array.from(Array(8))
-}
-export const emptyBoard = (): Board<undefined> => {
-    return Array.from(Array(8)).map(emptyRow);
-}
-
-type Row<T> = Array<T>
-
-const printBoard = (board: any) => {
-    const s = board.map((row: any) => row.map((cell: any) => cell?.piece?.name || '_').join('|').concat('|')).reverse().join('\n')
-    console.log()
-    console.log(s)
-    console.log()
-}
-
-export const isOnBoard = (coordinate: Coordinate) => {
-    return (coordinate.row <= 8 && coordinate.row >= 1 && coordinate.column >= 1 && coordinate.column <= 8)
-}
-
-
-export function deepCopyBoard<T>(board: Board<T>): Board<T> {
-    return board.map(arr => arr.slice())
-}
-
-export enum ColumnValues {
-    A = 1, B = 2, C = 3, D = 4, E = 5, F = 6, G = 7, H = 8
-}
-
-export function getBoardCell(board: Board<ActivePiece>, coord: Coordinate) {
-    return board[coord.row - 1][coord.column - 1]
-}
-
-
 export function createNewBoard(board: Board<ActivePiece>): NewBoard {
     return {
         board: board,
@@ -111,6 +77,39 @@ export function createNewBoard(board: Board<ActivePiece>): NewBoard {
 }
 
 
+
+export const emptyRow = (): Row<undefined> => {
+    return Array.from(Array(8))
+}
+export const emptyBoard = (): Board<undefined> => {
+    return Array.from(Array(8)).map(emptyRow);
+}
+
+type Row<T> = Array<T>
+
+const printBoard = (board: any) => {
+    const s = board.map((row: any) => row.map((cell: any) => cell?.piece?.name || '_').join('|').concat('|')).reverse().join('\n')
+    console.log()
+    console.log(s)
+    console.log()
+}
+
+export const isOnBoard = (coordinate: Coordinate) => {
+    return (coordinate.row <= 8 && coordinate.row >= 1 && coordinate.column >= 1 && coordinate.column <= 8)
+}
+
+
+export function deepCopyBoard<T>(board: Board<T>): Board<T> {
+    return board.map(arr => arr.slice())
+}
+
+export enum ColumnValues {
+    A = 1, B = 2, C = 3, D = 4, E = 5, F = 6, G = 7, H = 8
+}
+
+export function getBoardCell(board: Board<ActivePiece>, coord: Coordinate) {
+    return board[coord.row - 1][coord.column - 1]
+}
 
 export const createBoard = (pieces: Array<ActivePiece>): NewBoard => {
     const board: Board<ActivePiece> = emptyBoard()
